5f683b52530893f8d9c7f7d878841e6cf2ff22f8
Frederic Pillon <frederic.pillon@st.com>
2019-10-30 15:50:25 +0100

    Fix variant
    Main issues:
    * _estack size was wrong which I guess cause the board to hang at the boot
    * Pin misalignment due to PA_13 and PA_14 commented in digitalPin[].
    The PYn number is the index in the digitalPin[] so be careful it is well aligned.
    * wrong NUM_DIGITAL_PINS
    
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- variants/Example_STM32F373R8/variant.cpp
+++ variants/Example_STM32F373R8/variant.cpp
@@ -66,72 +66,13 @@ const PinName digitalPin[] = {
   PB_7, // nSwitch_10
   PA_9, // A13_SOM_RX = USART1_TX
   PA_10,  // A13_SOM_TX = USART1_RX
-  //PA_13,  // SWDIO
-  //PA_14,  // SWCLK
+  PA_13,  // SWDIO
+  PA_14,  // SWCLK
   PB_3, // SWO
   PC_13,  //LED??
   PA_0,
   PB_8
 };
-
-
-// static const uint8_t I_mirr_1mA = PC0;
-// static const uint8_t Temp_AN_1 = PC8;
-// static const uint8_t Temp_AN_2 = PC9;
-// static const uint8_t Temp_AN_3 = PA8;
-// static const uint8_t Temp_AN_4 = PA11;
-// static const uint8_t Temp_intern_AN = PA12;
-// static const uint8_t Temp_kal_3k_AN = PF7;
-// static const uint8_t Temp_kal_30k_AN = PF6;
-//
-// static const uint8_t AI24_AN_2 = PC4;
-// static const uint8_t AI24_AN_1 = PC5;
-// static const uint8_t AI24_AN_4 = PC6;
-// static const uint8_t AI24_AN_3 = PC7;
-// static const uint8_t o_20mA_1 = PA7;
-// static const uint8_t o_20mA_2 = PC1;
-//
-// static const uint8_t EN_DA_PWM_1 = PC2;
-// static const uint8_t EN_DA_PWM_2 = PC3;
-//
-// static const uint8_t AI_1 = PB1;
-// static const uint8_t AI_2 = PB2;
-// static const uint8_t AI_3 = PE8;
-// static const uint8_t AI_4 = PE9;
-// static const uint8_t AI_temperatur = PD8;
-// static const uint8_t AI_kal_3k = PB14;
-// static const uint8_t AI_kal_30k = PB15;
-// static const uint8_t AI_curr = PB0;
-//
-// static const uint8_t DI_1 = PA15;
-// static const uint8_t DI_2 = PD2;
-// static const uint8_t DI_3 = PC12;
-// static const uint8_t DI_4 = PC11;
-// static const uint8_t USART3_RX_1 = PC11;
-// static const uint8_t DI_5 = PB9;
-// static const uint8_t USART3_RX_2 = PB9;
-//
-// static const uint8_t USART3_TX = PC10;
-// static const uint8_t DA_PWM_1 = PA4;
-// static const uint8_t DA_PWM_2 = PA5;
-//
-// static const uint8_t pSwitch_7 = PA2;
-// static const uint8_t nSwitch_7 = PA1;
-// static const uint8_t pSwitch_8 = PA3;
-// static const uint8_t nSwitch_8 = PA6;
-// static const uint8_t pSwitch_9 = PB4;
-// static const uint8_t nSwitch_9 = PB6;
-// static const uint8_t pSwitch_10 = PB5;
-// static const uint8_t nSwitch_10 = PB7;
-//
-// static const uint8_t USART1_TX = PA9;
-// static const uint8_t USART1_RX = PA10;
-//
-// static const uint8_t SWDIO = PA13;
-// static const uint8_t SWCLK = PA14;
-// static const uint8_t SWO = PB3;
-
-
 
 #ifdef __cplusplus
 }
@@ -151,65 +92,34 @@ extern "C" {
 // void SystemClock_Config(void)
 WEAK void SystemClock_Config(void)
 {
-  //   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  //   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-  //   RCC_PeriphCLKInitTypeDef PeriphClkInit;
-  //
-  // //   /* Configure the main internal regulator output voltage */
-  // //   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-  //
-  // //   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  // //   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  // //   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-  //   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  //   RCC_OscInitStruct.HSEState = RCC_HSI_ON;
-  //   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-  // //   RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
-  //   //RCC_OscInitStruct.HSICalibrationValue = 16;
-  // //   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  // //   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  // //   RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
-  // //   RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV2;
-  //
-  //   //RCC_OscInitStruct.LSEState = RCC_LSE_ON;
-  //
-  //   HAL_RCC_OscConfig(&RCC_OscInitStruct);
-  //
-  //   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
-  //                                 | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-  // //   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSE; //RCC_SYSCLKSOURCE_PLLCLK;
-  //   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI; //RCC_SYSCLKSOURCE_PLLCLK;
-  //   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  //   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  //   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-  //   HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0);
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = {};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {};
 
-  /** Initializes the CPU, AHB and APB busses clocks
-  */
+  /* Initializes the CPU, AHB and APB busses clocks */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
     Error_Handler();
   }
-  /** Initializes the CPU, AHB and APB busses clocks
-  */
+  /* Initializes the CPU, AHB and APB busses clocks */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
                                 | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSE;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK) {
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
     Error_Handler();
   }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC1;
-  PeriphClkInit.Adc1ClockSelection = RCC_ADC1PCLK2_DIV2;
-
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB;
+  PeriphClkInit.USBClockSelection = RCC_USBCLKSOURCE_PLL_DIV1_5;
   if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
     Error_Handler();
   }
--- variants/Example_STM32F373R8/variant.h
+++ variants/Example_STM32F373R8/variant.h
@@ -29,7 +29,6 @@ extern "C" {
 
 
 #define PC0     0     // I_mirr_1mA
-
 #define PC8     1     // Temp_AN_1
 #define PC9     2     // Temp_AN_2
 #define PA8     3     // Temp_AN_3
@@ -37,17 +36,14 @@ extern "C" {
 #define PA12    5     // Temp_intern_AN
 #define PF7     6     // Temp_kal_3k_AN
 #define PF6     7     // Temp_kal_30k_AN
-
 #define PC4     8     // AI24_AN_2
 #define PC5     9     // AI24_AN_1
 #define PC6     10      // AI24_AN_4
 #define PC7     11      // AI24_AN_3
 #define PA7     12      // 0_20mA_1
 #define PC1     13      // 0_20mA_2
-
 #define PC2     14      // EN_DA_PWM_1
 #define PC3     15      // EN_DA_PWM_2
-
 #define PB1     16      // AI_1
 #define PB2     17      // AI_2
 #define PE8     18      // AI_3
@@ -56,17 +52,14 @@ extern "C" {
 #define PB14    21      // AI_kal_3k
 #define PB15    22      // AI_kal_30k
 #define PB0     23      // AI_curr
-
 #define PA15    24      // DI_1
 #define PD2     25      // DI_2
 #define PC12    26      // DI_3
 #define PC11    27      // DI_4, USART3_RX
 #define PB9     28      // DI_5, USART3_RX
-
 #define PC10    29      // USART3_TX
 #define PA4     30      // DA_PWM_1
 #define PA5     31      // DA_PWM_2
-
 #define PA2     32      // pSwitch_7
 #define PA1     33      // nSwitch_7
 #define PA3     34      // pSwitch_8
@@ -75,21 +68,18 @@ extern "C" {
 #define PB6     37      // nSwitch_9
 #define PB5     38      // pSwitch_10
 #define PB7     39      // nSwitch_10     40
-
 #define PA9     40      // USART1_TX = A13_SOM_RX
 #define PA10    41      // USART1_RX = A13_SOM_TX
-
 #define PA13    42      // SWDIO
 #define PA14    43      // SWCLK
 #define PB3     44      // SWO
-
 #define PC13    45
 #define PA0     46
 #define PB8     47
 
 
 // This must be a literal
-#define NUM_DIGITAL_PINS        60
+#define NUM_DIGITAL_PINS        48
 // This must be a literal with a value less than or equal to to MAX_ANALOG_INPUTS
 #define NUM_ANALOG_INPUTS       1
 #define NUM_ANALOG_FIRST        46
@@ -113,13 +103,12 @@ extern "C" {
 
 // Default pin used for 'Serial' instance (ex: ST-Link)
 // Mandatory for Firmata
-#define PIN_SERIAL_RX           41
-#define PIN_SERIAL_TX           40
-
+#define PIN_SERIAL_RX           PA10
+#define PIN_SERIAL_TX           PA9
 
 
 /* Extra HAL modules */
-//#define HAL_DAC_MODULE_ENABLED
+#define HAL_DAC_MODULE_ENABLED
 //#define HAL_SDADC_MODULE_ENABLED
 
 #ifdef __cplusplus
