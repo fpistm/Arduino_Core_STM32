5906143509b29de5463c5be0f91865b4276956bd
Frederic Pillon <frederic.pillon@st.com>
2021-03-15 09:26:10 +0100

    Add all generated STM32F0xx generic variant files
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

---
+++ variants/STM32F0xx/F072C(8-B)_F072CBYx/variant_generic.cpp
@@ -0,0 +1,73 @@
+/*
+ *******************************************************************************
+ * Copyright (c) 2020-2021, STMicroelectronics
+ * All rights reserved.
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ *******************************************************************************
+ */
+#if defined(ARDUINO_GENERIC_F072C8TX) || defined(ARDUINO_GENERIC_F072C8UX) ||\
+    defined(ARDUINO_GENERIC_F072CBTX) || defined(ARDUINO_GENERIC_F072CBUX) ||\
+    defined(ARDUINO_GENERIC_F072CBYX)
+#include "pins_arduino.h"
+
+// Digital PinName array
+const PinName digitalPin[] = {
+  PA_0,
+  PA_1,
+  PA_2,
+  PA_3,
+  PA_4,
+  PA_5,
+  PA_6,
+  PA_7,
+  PA_8,
+  PA_9,
+  PA_10,
+  PA_11,
+  PA_12,
+  PA_13,
+  PA_14,
+  PA_15,
+  PB_0,
+  PB_1,
+  PB_2,
+  PB_3,
+  PB_4,
+  PB_5,
+  PB_6,
+  PB_7,
+  PB_8,
+  PB_9,
+  PB_10,
+  PB_11,
+  PB_12,
+  PB_13,
+  PB_14,
+  PB_15,
+  PC_13,
+  PC_14,
+  PC_15,
+  PF_0,
+  PF_1
+};
+
+// Analog (Ax) pin number array
+const uint32_t analogInputPin[] = {
+  0,  // A0,  PA0
+  1,  // A1,  PA1
+  2,  // A2,  PA2
+  3,  // A3,  PA3
+  4,  // A4,  PA4
+  5,  // A5,  PA5
+  6,  // A6,  PA6
+  7,  // A7,  PA7
+  16, // A8,  PB0
+  17  // A9,  PB1
+};
+
+#endif /* ARDUINO_GENERIC_* */
