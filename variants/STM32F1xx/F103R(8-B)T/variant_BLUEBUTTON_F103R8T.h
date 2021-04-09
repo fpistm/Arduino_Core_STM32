7193254e86a21fa16830bd991be5cdfd9c67669e
Alexandre Bourdiol <alexandre.bourdiol@st.com>
2021-04-09 19:22:33 +0200

    [variant] Add Generic F1xx
    Signed-off-by: Alexandre Bourdiol <alexandre.bourdiol@st.com>

---
+++ variants/STM32F1xx/F103R(8-B)T/variant_BLUEBUTTON_F103R8T.h
@@ -0,0 +1,72 @@
+/*
+ *******************************************************************************
+ * Copyright (c) 2011-2021, STMicroelectronics
+ * All rights reserved.
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ *******************************************************************************
+ */
+#pragma once
+
+// This header file override few definitions and then include variant_generic.h
+
+// On-board LED pin number
+#define LED_GREEN               PB11
+#ifndef LED_BUILTIN
+  #define LED_BUILTIN           LED_GREEN
+#endif
+
+// On-board user button
+#ifndef USER_BTN
+  #define USER_BTN              PC13
+#endif
+
+// Timer Definitions
+#ifndef TIMER_SERVO
+  #define TIMER_SERVO           TIM2
+#endif
+
+// UART Definitions
+// Define here Serial instance number to map on Serial generic name
+#ifndef SERIAL_UART_INSTANCE
+  #define SERIAL_UART_INSTANCE  1
+#endif
+
+// Default pin used for 'Serial1' instance
+#ifndef PIN_SERIAL_RX
+  #define PIN_SERIAL_RX         PA10
+#endif
+#ifndef PIN_SERIAL_TX
+  #define PIN_SERIAL_TX         PA9
+#endif
+
+/*----------------------------------------------------------------------------
+ *        Arduino objects - C++ only
+ *----------------------------------------------------------------------------*/
+
+#ifdef __cplusplus
+  // These serial port names are intended to allow libraries and architecture-neutral
+  // sketches to automatically default to the correct port name for a particular type
+  // of use.  For example, a GPS module would normally connect to SERIAL_PORT_HARDWARE_OPEN,
+  // the first hardware serial port whose RX/TX pins are not dedicated to another use.
+  //
+  // SERIAL_PORT_MONITOR        Port which normally prints to the Arduino Serial Monitor
+  //
+  // SERIAL_PORT_USBVIRTUAL     Port which is USB virtual serial
+  //
+  // SERIAL_PORT_LINUXBRIDGE    Port which connects to a Linux system via Bridge library
+  //
+  // SERIAL_PORT_HARDWARE       Hardware serial port, physical RX & TX pins.
+  //
+  // SERIAL_PORT_HARDWARE_OPEN  Hardware serial ports which are open for use.  Their RX & TX
+  //                            pins are NOT connected to anything by default.
+  #ifndef SERIAL_PORT_HARDWARE
+    #define SERIAL_PORT_HARDWARE  Serial1
+  #endif
+#endif
+
+#include "variant_generic.h"
---
+++ variants/STM32F1xx/F103Z(C-D-E)(H-T)/variant_generic_VCCGND_F103ZET6_MINI.h
@@ -0,0 +1,99 @@
+/*
+ *******************************************************************************
+ * Copyright (c) 2019, STMicroelectronics
+ * All rights reserved.
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ *******************************************************************************
+ */
+
+// This header file override few definitions and then include variant_generic.h
+
+// On-board LED pin number
+#ifndef LED_BUILTIN
+  #define LED_BUILTIN           PG15
+#endif
+
+// On-board user button
+#ifndef USER_BTN
+  #define USER_BTN              PB1
+#endif
+
+// SPI Definitions
+#ifndef PIN_SPI_SS
+  #define PIN_SPI_SS            PC4
+#endif
+
+// UART Definitions
+// Define here Serial instance number to map on Serial generic name
+#ifndef SERIAL_UART_INSTANCE
+  #define SERIAL_UART_INSTANCE  1
+#endif
+
+// Default pin used for 'Serial' instance (ex: ST-Link)
+// Mandatory for Firmata
+#ifndef PIN_SERIAL_RX
+  #define PIN_SERIAL_RX         PA10
+#endif
+#ifndef PIN_SERIAL_TX
+  #define PIN_SERIAL_TX         PA9
+#endif
+
+// Default pin used for 'Serial2' instance
+#ifndef PIN_SERIAL2_RX
+  #define PIN_SERIAL2_RX         PA3
+#endif
+#ifndef PIN_SERIAL2_TX
+  #define PIN_SERIAL2_TX         PA2
+#endif
+
+// Default pin used for 'Serial3' instance
+#ifndef PIN_SERIAL3_RX
+  #define PIN_SERIAL3_RX         PB11
+#endif
+#ifndef PIN_SERIAL3_TX
+  #define PIN_SERIAL3_TX         PB10
+#endif
+
+// Extra HAL modules
+#if !defined(HAL_DAC_MODULE_DISABLED)
+  #define HAL_DAC_MODULE_ENABLED
+#endif
+#if !defined(HAL_SD_MODULE_DISABLED)
+  #define HAL_SD_MODULE_ENABLED
+#endif
+
+// SD card slot Definitions
+// SD detect signal can be defined if required
+#define SD_DETECT_PIN           PF10
+
+/*----------------------------------------------------------------------------
+ *        Arduino objects - C++ only
+ *----------------------------------------------------------------------------*/
+
+#ifdef __cplusplus
+  // These serial port names are intended to allow libraries and architecture-neutral
+  // sketches to automatically default to the correct port name for a particular type
+  // of use.  For example, a GPS module would normally connect to SERIAL_PORT_HARDWARE_OPEN,
+  // the first hardware serial port whose RX/TX pins are not dedicated to another use.
+  //
+  // SERIAL_PORT_MONITOR        Port which normally prints to the Arduino Serial Monitor
+  //
+  // SERIAL_PORT_USBVIRTUAL     Port which is USB virtual serial
+  //
+  // SERIAL_PORT_LINUXBRIDGE    Port which connects to a Linux system via Bridge library
+  //
+  // SERIAL_PORT_HARDWARE       Hardware serial port, physical RX & TX pins.
+  //
+  // SERIAL_PORT_HARDWARE_OPEN  Hardware serial ports which are open for use.  Their RX & TX
+  //                            pins are NOT connected to anything by default.
+  #ifndef SERIAL_PORT_HARDWARE
+    #define SERIAL_PORT_HARDWARE  Serial1
+  #endif
+#endif
+
+#include "variant_generic.h"
