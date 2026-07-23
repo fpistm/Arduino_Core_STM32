bb4b804b8c4a0dbcbe16e09e38e973e52bc2cebc
Frederic Pillon <frederic.pillon@st.com>
2026-07-23 15:26:27 +0200

    chore: update core version to 3.0.1-dev (0x030001F0)
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- libraries/SrcWrapper/inc/stm32_def.h
+++ libraries/SrcWrapper/inc/stm32_def.h
@@ -7,14 +7,14 @@
  */
 #define STM32_CORE_VERSION_MAJOR    (0x03U) /*!< [31:24] major version */
 #define STM32_CORE_VERSION_MINOR    (0x00U) /*!< [23:16] minor version */
-#define STM32_CORE_VERSION_PATCH    (0x00U) /*!< [15:8]  patch version */
+#define STM32_CORE_VERSION_PATCH    (0x01U) /*!< [15:8]  patch version */
 /*
  * Extra label for development:
  * 0: official release
  * [1-9]: release candidate
  * F[0-9]: development
  */
-#define STM32_CORE_VERSION_EXTRA    (0x00U) /*!< [7:0]  extra version */
+#define STM32_CORE_VERSION_EXTRA    (0x0FU) /*!< [7:0]  extra version */
 #define STM32_CORE_VERSION          ((STM32_CORE_VERSION_MAJOR << 24U)\
                                         |(STM32_CORE_VERSION_MINOR << 16U)\
                                         |(STM32_CORE_VERSION_PATCH << 8U )\
